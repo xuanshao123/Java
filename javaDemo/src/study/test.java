e0b511a4bb20f5f641614fe472f06865298d7cd8
admin <admin@sademail>
2019-01-09 18:17:12 +0800

    第一次提交

---
+++ javaDemo/src/study/test.java
@@ -0,0 +1,13 @@
+package study;
+
+/**
+ * @Auther: Teach_Itman
+ * @Date: 2019/1/9
+ * @Description: study
+ * @version:
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
